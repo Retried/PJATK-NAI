8491ece22b5060b84e57a5ef45237431de783c95
Igor Doga <Igorasek16@gmail.com>
2022-10-19 17:05:54 +0200

    Lab 2, almost complete

--- Lab 2/cmake-build-debug/cmake_install.cmake
+++ Lab 2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: Z:/NAI Lab/Lab 2
+# Install script for directory: C:/Users/Igora/Desktop/PJATK-NAI/Lab 2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "Z:/NAI Lab/Lab 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Igora/Desktop/PJATK-NAI/Lab 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -2,61 +2,83 @@
 #include <vector>
 #include <functional>
 #include <random>
-/**
- domain - generate domain points. Throws exception when all the points were returned
-**/
+#include <chrono>
 
-//Placeholder ;)
+using namespace std;
 
-auto brute_force = [](auto f, auto domain) {
+auto brute_force = [](auto f, auto domain, auto pool) {
     auto current_p = domain();
     auto best_point = current_p;
     try {
-        while (true) {
+        for (int i = 0; i < pool; i++) {
             if (f(current_p) < f(best_point)) {
                 best_point = current_p;
             }
             current_p = domain();
         }
-    } catch (std::exception &e) {
+    } catch (exception &e) {
     }
     return best_point;
 };
 
-using domain_t = std::vector<double>;
-std::random_device rd;
-std::mt19937 mt_generator(rd());
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
-    for (int i = 0; i < minimal_d.size(); i++) {
-        std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0/128.0, 1.0/128.0);
-            new_p[i] = current_p[i] + dist(mt_generator);
-        }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
-    }
-    return current_p;
-}
+using domain_t = vector<double>;
+random_device rd;
+mt19937 mt_generator(rd());
 
 int main() {
-    auto sphere_f = [](double x) {return x*x;};
-    double current_sphere_x = -10;
+    auto xy_gen = [&](){
+        uniform_real_distribution<> dis(-10,10);
+        return pair<double, double> (dis(mt_generator), dis(mt_generator));
+    };
+
+    auto sphere_f = [](pair<double, double> pair) {
+        return pow(pair.first,2) + pow(pair.second,2);
+    };
+
+    auto booth_f = [](pair<double, double> pair) {
+        return pow((pair.first + (2 * pair.second) - 7),2) + pow(((2*pair.first) + pair.second - 5),2);
+    };
+
+    auto matyas_f = [](pair<double, double> pair) {
+        return 0.26 * (pow(pair.first,2) + pow(pair.second,2)) - (0.48 * pair.first * pair.second);
+    };
+
+    /*double current_sphere_x = -10;
     auto sphere_generator = [&]() {
         current_sphere_x+= 1.0/128.0;
-        if (current_sphere_x >= 10) throw std::invalid_argument("finished");
+        if (current_sphere_x >= 10) throw invalid_argument("finished");
         return current_sphere_x;
-    };
-    auto best_point = brute_force(sphere_f, sphere_generator);
-    std::cout << "best x = " << best_point << std::endl;
-    auto sphere_f_v = [](domain_t x) {return x[0]*x[0];};
-    auto best2 = hill_climbing(sphere_f_v, {-10},{10},10000);
-    std::cout << "best x = " << best2[0] << std::endl;
+    };*/
+
+    auto time_start = chrono::high_resolution_clock::now();
+    auto best_point = brute_force(booth_f, xy_gen, 1000);
+    auto time_stop = chrono::high_resolution_clock::now();
+    cout << "Sphere function" << endl;
+    cout << "best x = " << best_point.first << endl;
+    cout << "best y = " << best_point.second << endl;
+    cout << "result = " << booth_f(best_point) << endl;
+    cout << "time = " << chrono::duration_cast<chrono::microseconds>(time_stop - time_start).count() << endl;
+
+    cout << "------------------------" << endl;
+
+    time_start = chrono::high_resolution_clock::now();
+    best_point = brute_force(sphere_f, xy_gen, 1000);
+    time_stop = chrono::high_resolution_clock::now();
+    cout << "Booth function" << endl;
+    cout << "best x = " << best_point.first << endl;
+    cout << "best y = " << best_point.second << endl;
+    cout << "result = " << sphere_f(best_point) << endl;
+    cout << "time = " << chrono::duration_cast<chrono::microseconds>(time_stop - time_start).count() << endl;
+
+    cout << "------------------------" << endl;
+
+    time_start = chrono::high_resolution_clock::now();
+    best_point = brute_force(matyas_f, xy_gen, 1000);
+    time_stop = chrono::high_resolution_clock::now();
+    cout << "Matyas function" << endl;
+    cout << "best x = " << best_point.first << endl;
+    cout << "best y = " << best_point.second << endl;
+    cout << "result = " << matyas_f(best_point) << endl;
+    cout << "time = " << chrono::duration_cast<chrono::microseconds>(time_stop - time_start).count() << endl;
     return 0;
 }
